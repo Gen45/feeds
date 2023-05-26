92bfc03e03fcfb40737d6eb39a983b47c92dd0db
Genaro De Sia Coppola <genarodesia@gmail.com>
2023-05-26 19:02:29 -0300

    setup ESLint, prettier, TS Config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "assets": path.resolve(__dirname, "./src/assets"),
+      "components": path.resolve(__dirname, "./src/components"),
+      "views": path.resolve(__dirname, "./src/views"),
+      "models": path.resolve(__dirname, "./src/models"),
+      "utils": path.resolve(__dirname, "./src/utils"),
+      "services": path.resolve(__dirname, "./src/services"),
     },
   },
 })
